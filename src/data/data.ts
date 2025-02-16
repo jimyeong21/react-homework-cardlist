1a485477ac017ca082efd7bdc38698f6d211712f
jimyeong21 <jimyeong21@gmail.com>
2025-02-16 18:25:34 +0900

    [데이터] 숙소 데이터 및 이미지

---
+++ src/data/data.ts
@@ -0,0 +1,52 @@
+const villaList = [
+  {
+    id: 'villa001',
+    title: '양평숲속별장',
+    description: '핀란드 사우나와 야외 수영장이 있는 양평숲속별장',
+    image: '/villa/villa001.avif',
+    tags: ['양평', '수영장', '🐶 반려동물가능'],
+    isFavorited: false,
+  },
+  {
+    id: 'villa002',
+    title: '마스테이',
+    description: '도심 속 휴식공간에서 감성 가득한 시간을 가져보세요',
+    image: '/villa/villa002.avif',
+    tags: ['서울'],
+    isFavorited: false,
+  },
+  {
+    id: 'villa003',
+    title: '한옥스테이 <무렵>',
+    description: '고즈넉한 그때가 당신의 기억에 머물 즈음',
+    image: '/villa/villa003.avif',
+    tags: ['전주', '한옥'],
+    isFavorited: false,
+  },
+  {
+    id: 'villa004',
+    title: '미르의 뜰',
+    description: '평온한 숙소에서 온 가족이 함께 휴식 시간을 가져보세요',
+    image: '/villa/villa004.avif',
+    tags: ['파주', '🐶 반려동물가능'],
+    isFavorited: false,
+  },
+  {
+    id: 'villa005',
+    title: '사계절이 아름다운 집',
+    description: '조용한 힐링을 원하신다면 넓은 정원이 있는 안락한 양평',
+    image: '/villa/villa005.avif',
+    tags: ['양평', '자연', '🐶 반려동물가능'],
+    isFavorited: false,
+  },
+  {
+    id: 'villa006',
+    title: '바다와 정원이 있는 별장',
+    description: '편안하고 소박한 일상의 집',
+    image: '/villa/villa006.avif',
+    tags: ['삼척', '바다'],
+    isFavorited: false,
+  },
+];
+
+export default villaList;
